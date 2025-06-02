d0ba4227461fcfe03bff22306d78c1e3ac76b5b3
RIZZIALPADISTA <161547478+RIZZIALPADISTA@users.noreply.github.com>
2025-06-02 15:14:43 +0700

    Add files via upload

---
+++ Data Mahasiswa/src/Mahasiswa23E.java
@@ -0,0 +1,8 @@
+public class Mahasiswa23E {
+
+    public static void main(String[] args) {
+        Mahasiswa m23 = new Mahasiswa();
+        m23.readMahasiswa();
+        m23.insertMahasiswa("20230040042", "Budi", "Nusa Putra", "L");
+    }
+}
